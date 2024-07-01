afc468369da22128dbd7eb3db4405f7e643a28f8
eyamagishi <e.yamagishi0625@gmail.com>
2024-07-01 12:53:34 +0900

    fix: ホームページ名を変更

--- resources/ts/config/constants.ts
+++ resources/ts/config/constants.ts
@@ -1,5 +1,5 @@
 // constants.ts
-export const HOMEPAGE_NAME: string = "徒然なるままに";
+export const HOMEPAGE_NAME: string = "Done is better than perfect";
 export const HOME: string = "HOME";
 export const ABOUT: string = "ABOUT";
 export const CONTACT: string = "CONTACT";

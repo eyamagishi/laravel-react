fc51f31441c7fa75764b40f8a673d6b28b03e4e0
eyamagishi <e.yamagishi0625@gmail.com>
2024-07-01 13:26:35 +0900

    add: axiosを追加

--- resources/ts/components/Todo.tsx
+++ resources/ts/components/Todo.tsx
@@ -1,12 +1,48 @@
-import React from 'react';
+import React, { useEffect, useState } from "react";
+import axios from "axios";
 
 import { TODO } from '../config/constants';
 
-const Todo: React.FC = () => (
-    <section id="todo">
-        <h2>{TODO}</h2>
-        <p>ここはTODOのセクションです。</p>
-    </section>
-);
+interface TodoItem {
+    id: number;
+    title: string;
+    description: string;
+    completed: boolean;
+}
+
+const Todo: React.FC = () => {
+    const [todos, setTodos] = useState<TodoItem[]>([]);
+
+    const fetchTodos = async () => {
+        try {
+            const res = await axios.get<TodoItem[]>('api/todos');
+            setTodos(res.data);
+        } catch (error) {
+            console.error('Failed to fetch todos:', error);
+            return error;
+        }
+    };
+
+    useEffect(() => {
+        fetchTodos();
+    }, []);
+
+    return (
+        <section id="todo">
+            <h2>{TODO}</h2>
+            <p>ここはTODOのセクションです。</p>
+            {todos.map((todo) => {
+                return (
+                    <div key={todo.id}>
+                        <h1>title:{todo.title}</h1>
+                        <p>id:{todo.id}</p>
+                        <p>description:{todo.description}</p>
+                        <p>completed:{todo.completed}</p>
+                    </div>
+                );
+            })}
+        </section>
+    );
+};
 
 export default Todo;
